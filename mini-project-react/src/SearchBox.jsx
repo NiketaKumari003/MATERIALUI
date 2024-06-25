c2e3cf1157dc8b358bf8f8a338de02032e42fb0c
delta-demo <niketa78709846@gmail.com>
2024-06-26 00:04:49 +0530

    files modified

--- mini-project-react/src/SearchBox.jsx
+++ mini-project-react/src/SearchBox.jsx
@@ -4,6 +4,29 @@ import "./SearchBox.css";
 import { useState } from 'react';
 export default function SearchBox(){
     let [city,setCity]=useState("");
+    const API_URL="https://api.openweathermap.org/data/3.0/onecall"
+    const API_KEY="f3459835be758fcaaf8a0b5f3146a878";
+
+
+let getWeatherInfo= async ()=>{
+
+   let response
+   = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
+
+
+let jsonResponse=await response.json();
+console.log(jsonResponse);
+
+
+
+};
+
+
+
+
+
+
+    
     let handleChange=(evt)=>{
 setCity(evt.target.value);
     };
@@ -11,6 +34,7 @@ setCity(evt.target.value);
         evt.preventDefault();
         console.log(city);
         setCity("");
+        getWeatherInfo();
     }
     return (
         <div className="SearchBox">
